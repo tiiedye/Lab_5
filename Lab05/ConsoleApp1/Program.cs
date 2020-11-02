c49da0e9759e342be34c95ed3f203af85771bb5b
tiiedye <tiiedye@gmail.com>
2020-11-02 12:03:10 -0800

    adding provided source code

--- Lab05/ConsoleApp1/Program.cs
+++ Lab05/ConsoleApp1/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Fibonacci(int n)
         {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+        static void Main()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.Write(Fibonacci(i));
+                Console.Write(", ");
+            }
+            Console.ReadKey();
         }
     }
 }
